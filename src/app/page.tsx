bfd4c89623f10c55a31923de72d86dac08ccbc46
dawsdo <dsdo@uab.edu>
2026-06-23 23:51:23 -0500

    Add selected work to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/Hero";
+import SelectedWorkSection from "@/components/SelectedWorkSection";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <SelectedWorkSection />
     </main>
   );
 }
